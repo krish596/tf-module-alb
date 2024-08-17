705b6a425ef22c1434b929cbd38f4c23a4b84618
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 23:27:03 +0530

    Terraform

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  tags = merge(var.tags, {tf-module-name = "vpc"}, {env = var.env})
+}
--- main.tf
+++ main.tf
@@ -4,11 +4,7 @@ resource "aws_lb" "main" {
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-
-
-  tags = {
-    Environment = "production"
-  }
+  tags = merge(local.tags, {Name = "${var.env}-alb"})
 }
 
 resource "aws_security_group" "main" {
@@ -16,9 +12,7 @@ resource "aws_security_group" "main" {
   description = "${var.env}-alb-sg"
   vpc_id      = var.vpc_id
 
-  tags = {
-    Name = "allow_tls"
-  }
+  tags = merge(local.tags, {Name = "${var.env}-alb-sg"})
 
   ingress {
     description      = "APP"
