42e4c280f61f0864784874cfef62e9b7072e25c5
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 14:14:29 +0530

    Terraform

--- locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  # Tags
-  tags = merge(var.tags, {tf-module-name = "alb"}, {env = var.env})
-
-  lb_name = var.internal ? "${var.env}-alb-internal" : "${var.env}-alb-public"
-}
--- vars.tf
+++
@@ -1,8 +0,0 @@
-variable "tags" {}
-variable "env" {}
-variable "internal" {}
-variable "lb_type" {}
-variable "sg_ingress_cidr" {}
-variable "vpc_id" {}
-variable "sg_port" {}
-variable "subnets" {}
