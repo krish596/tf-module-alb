461ef3b50d484296b9f6969637aed2e8abeacd2c
Chimtamreddy <kr7348202@gmail.com>
2024-08-20 08:45:07 +0530

    Terraform

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "alb" {
-  value = aws_lb.main
-}
-
-output "listener" {
-  value = aws_lb_listener.main
-}
+# output "alb" {
+#   value = aws_lb.main
+# }
+#
+# output "listener" {
+#   value = aws_lb_listener.main
+# }
