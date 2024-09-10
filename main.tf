8cc68dfb6ac89e27cb93118c7605d37d9418818f
Chimtamreddy <kr7348202@gmail.com>
2024-09-10 19:46:43 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -66,3 +66,12 @@ resource "aws_security_group" "main" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_security_group_rule" "frontend" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = var.sg_ingress_cidr
+  security_group_id = aws_security_group.main.id
+}
