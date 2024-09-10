0d4a3220447bb44f29d280f67690927b35164d29
Chimtamreddy <kr7348202@gmail.com>
2024-09-10 19:37:15 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -25,22 +25,7 @@ resource "aws_lb_listener" "main" {
   }
 }
 
-resource "aws_lb_listener" "main" {
-  count = var.internal ? 0 : 1
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
 
 resource "aws_security_group" "main" {
   name        = local.sg_name
